547f870824d4a4d94ced463b4b1cdb0c9c498c25
SiMoG97 <echaaranimohamed@gmail.com>
2023-06-05 22:43:22 -0700

    task-13: adding lists.h, README.md and 13-insert_number.c files

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list
+ * @head: Pointer to the head of the linked list
+ * @number: Number to be inserted into the linked list
+ *
+ * Return: Pointer to the newly inserted node, or NULL on failure
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *h = *head;
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = number;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+
+	if (number <= h->n)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
+	while (h->next)
+	{
+		if (number <= h->next->n)
+		{
+			newNode->next = h->next;
+			h->next = newNode;
+			return (newNode);
+		}
+		h = h->next;
+	}
+	return (NULL);
+}
