fadfd56572da6e5a6335940584d473ae9c5ad987
SiMoG97 <echaaranimohamed@gmail.com>
2023-06-06 11:49:12 -0700

    task-6: fixing format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for l in range(0, 10):
     for r in range (l + 1, 10):
         if (l == 8 and r == 9):
-            print(f"{l}{r}")
+            print("{}{}".format(l, r))
             continue
-        print(f"{l}{r}", end = ", ")
+        print("{}{}".format(l, r), end = ", ")
 
