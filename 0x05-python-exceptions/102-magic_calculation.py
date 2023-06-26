e29604b9bce1c99784f2852bd0333c2c0449c7ac
SiMoG97 <echaaranimohamed@gmail.com>
2023-06-26 07:54:44 -0700

    trying to fix 102 file

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,9 +7,8 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception("Too far")
-            else:
-                result += (a ** b) / i
-        except Exception:
+            result += (a ** b) / i
+        except:
             result += a + b
             break
 
