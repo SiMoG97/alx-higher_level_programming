385d738a9d4b3847660d8abb43d7a04cb5c21d10
SiMoG97 <echaaranimohamed@gmail.com>
2023-06-27 18:02:20 -0700

    Task-3: adding 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class and functions related to squares.
+"""
+
+
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square object with the specified size.
+
+        Args:
+            size: The size (length of a side) of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
