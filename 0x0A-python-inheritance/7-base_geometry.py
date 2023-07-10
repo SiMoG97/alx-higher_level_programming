462af048743b13e150f2ef20806ac341d8e5197b
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-10 14:04:44 -0700

    TAsk-7: adding 7-base_heometry.py 7-base_geometry.txt

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises Exception
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method that raises TypeError and ValueError
+
+        Args:
+            name (str): name
+            value (int): value
+
+        Raises:
+            TypeError: <name> must be an integer
+            ValueError: <name> must be greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0")
