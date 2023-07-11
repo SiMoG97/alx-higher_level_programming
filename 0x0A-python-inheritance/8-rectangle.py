7eae3e1d21384f48556e3109a003e6a2c63904ae
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-10 17:48:46 -0700

    Task-8: fixing all errors

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Rectangle class"""
-Rectangle = __import__('8-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
