210c44ebf88406b980d55079a09c671d443d493e
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-11 10:14:19 -0700

    Task-5: adding 5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""save_to_json_file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation:
+
+    Args:
+        my_obj (any): the object to write
+        filename (any): file to be written at
+
+    Returns:
+        int: the number of characters written
+    """
+    with open(filename, mode="a") as f:
+        return f.write(json.dumps(my_obj))
