a05e8040585ae62cccfb8d250c34dc4c1d32a11e
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-11 14:13:23 -0700

    Task-7: pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 if __name__ == "__main__":
     filename = "add_item.json"
     args = list(sys.argv[1:])
-    
+
     data = []
     if os.path.isfile(filename):
         data = list(load_from_json_file(filename))
