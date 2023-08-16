dd5bc5b66a5351a9535a7469d5cf319b62e51206
SiMoG97 <echaaranimohamed@gmail.com>
2023-08-16 10:25:42 -0700

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 -- If a show doesn’t have a genre, display NULL in the genre column
 SELECT tvs.title, tvg.name
-FROM tv_shows as tvs
+FROM tv_shows AS tvs
 	LEFT JOIN tv_show_genres AS tvsg
 		ON tvs.id = tvsg.show_id
 	LEFT JOIN tv_genres AS tvg
