dc26467ffaf8d8a6fe4896aad62892dded59593e
SiMoG97 <echaaranimohamed@gmail.com>
2023-08-16 08:41:35 -0700

    Task-10: 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
 SELECT tvs.title, tvsg.genre_id
 FROM tv_shows AS tvs
 JOIN tv_show_genres AS tvsg
