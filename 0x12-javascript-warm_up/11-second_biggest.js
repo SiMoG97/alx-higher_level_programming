122e49806336da1926fbe6cb11d935c6e9af2854
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-12 02:50:41 +0100

    fix error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,11 +7,12 @@ if (len === 2 || len === 3) {
   console.log(0);
 } else {
   let biggest = parseInt(args[2], 10);
-  let secBiggest = parseInt(args[2], 10);
+  let secBiggest = parseInt(args[3], 10);
 
   for (let i = 3; i < len; i++) {
     const number = parseInt(args[i], 10);
     if (number > biggest) {
+      secBiggest = biggest;
       biggest = number;
     } else if (number < biggest && number > secBiggest) {
       secBiggest = number;
