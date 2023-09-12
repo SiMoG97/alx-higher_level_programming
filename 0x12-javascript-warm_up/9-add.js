0b004d55e6d9987f4345909bb74018d6131c939c
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-12 02:00:09 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv: args } = require('process');
+
+const [num1, num2] = [parseInt(args[2], 10), parseInt(args[3], 10)];
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(num1, num2));
