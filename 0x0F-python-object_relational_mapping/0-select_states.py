1410121e61af1b5476dc4ffd85b84e686682531a
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-15 18:07:00 +0100

    fixing pycodesytyle task-0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@ import sys
 
 args = sys.argv
 conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=args[1], passwd=args[2], db=args[3], charset="utf8")
+                       user=args[1], passwd=args[2],
+                       db=args[3], charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
