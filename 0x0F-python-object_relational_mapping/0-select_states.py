616481126ad4f03658b751cbaa839458d04e6552
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-15 18:13:21 +0100

    trying to fix the requirements

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""0-select_states"""
+"""print all states from hbtn_0e_0_usa database"""
 import MySQLdb
 import sys
 
-args = sys.argv
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=args[1], passwd=args[2],
-                       db=args[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+
+def printAllStates():
+    args = sys.argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3],
+                         charset="utf8"
+                         )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    printAllStates()
