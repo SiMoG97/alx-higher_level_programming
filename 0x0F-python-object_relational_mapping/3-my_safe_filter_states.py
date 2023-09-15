6879c4f062e17b62f6596b1614837a76135ba953
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-15 19:40:58 +0100

    Task-3: adding 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+   a script that takes in an argument and displays all values
+   in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3],
+                         charset="utf8"
+                         )
+    cur = db.cursor()
+    statement = """SELECT * FROM states
+                    WHERE BINARY name = %s
+                    ORDER BY id ASC
+                """
+    cur.execute(statement, (args[4],))
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
