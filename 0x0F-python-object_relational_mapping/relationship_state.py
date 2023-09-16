1f0c3b9eb0a500eaa50c37e3304ea5aacf407f7a
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-16 18:46:11 +0100

    Task-15: 100-relationship_states_cities.py relationship_city.py relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Module that defines a State class
+    that represents a states table in the database
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that represents a states table in the database
+
+    Attributes:
+        id (int): state id
+        name (str): state name
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete, delete-orphan")
