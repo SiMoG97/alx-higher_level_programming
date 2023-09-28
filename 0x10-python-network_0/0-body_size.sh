dd2ae202878bd4b68c7388bcad6eb14e0073fed4
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-28 22:13:29 +0100

    Task-0: removing args checking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-if [[ $# -lt 1 ]]; then
-    exit 1
-fi
 url="$1"
 curl -s -o - "$url" | wc -c 
