19936386edfb760eb649c951f1c4926089646e88
SiMoG97 <echaaranimohamed@gmail.com>
2023-09-29 15:33:27 +0100

    Task-0: fixing urllib import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""_summary_
-"""
-from urllib import request
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with request.urlopen(url) as res:
+    with urllib.request.urlopen(url) as res:
         data = res.read()
         decodedData = data.decode("UTF-8")
 
         print("Body response:")
-        print(f"    - type: {type(data)}")
-        print(f"    - content: {data}")
-        print(f"    - utf8 content: {decodedData}")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {decodedData}")
