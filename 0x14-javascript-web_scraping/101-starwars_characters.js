76862ef83c385ebb79b561b3529055d79195830e
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-24 17:57:08 +0100

    Task-8: 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,8 +3,23 @@ const { argv } = process;
 const request = require('request');
 
 if (process.argv.length < 3) {
-  console.error('Usage: node 100-starwars_characters.js <Movie-ID>');
+  console.error('Usage: node 101-starwars_characters.js <Movie-ID>');
   process.exit(1);
+}
+
+function fetchCharacters (url) {
+  return new Promise(
+    (resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        const character = JSON.parse(body).name;
+        resolve(character);
+      });
+    }
+  );
 }
 
 const movieID = argv[2];
@@ -15,20 +30,14 @@ request(`https://swapi-api.alx-tools.com/api/films/${movieID}`, function (err, r
   }
   try {
     const characters = JSON.parse(data).characters;
-    for (const chara of characters) {
-      request(chara, function (error, response, body) {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        try {
-          const characName = JSON.parse(body).name;
-          console.log(characName);
-        } catch (e) {
-          console.error(e);
-        }
+    const promises = characters.map(url => fetchCharacters(url));
+    Promise.all(promises).then((res) => {
+      res.forEach((name) => {
+        console.log(name);
       });
-    }
+    }).catch(e => {
+      console.log(e);
+    });
   } catch (e) {
     console.error(e);
   }
