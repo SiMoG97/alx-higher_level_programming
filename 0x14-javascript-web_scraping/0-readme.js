a04971b529907b25800af4266d0fb6eebbc454c2
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-24 14:11:05 +0100

    Task-0: fixing the error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,15 @@
 const { argv } = require('node:process');
 const fs = require('fs');
 
+if (process.argv.length < 3) {
+  console.error('Usage: node 0-readme.js <file-path>');
+  process.exit(1);
+}
+
 fs.readFile(argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
