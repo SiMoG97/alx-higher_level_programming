28be5c7eb02be736900952157268e58b2093e363
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-24 15:34:27 +0100

    Task-6: 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const { argv } = process;
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.error('Usage: node 6-completed_tasks.js <URL>');
+  process.exit(1);
+}
+
+request(argv[2], function (err, res, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const todos = JSON.parse(data);
+
+  const completedTasks = todos.reduce((compTasks, currTask) => {
+    const uid = currTask.userId;
+
+    if (isNaN(compTasks[uid])) {
+      compTasks[uid] = 0;
+    }
+    currTask.completed && compTasks[uid]++;
+    return compTasks;
+  }, {});
+
+  console.log(completedTasks);
+});
