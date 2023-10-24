4703c6b75a91b9e1231996f0bb2207be1862bf60
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-24 15:57:09 +0100

    Task-6: maybe i found the problem

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,23 +2,17 @@
 const { argv } = process;
 const request = require('request');
 
-// if (process.argv.length < 3) {
-//   console.error('Usage: node 6-completed_tasks.js <URL>');
-//   process.exit(1);
-// }
-
 request(argv[2], function (err, res, data) {
   if (err) {
     console.error(err);
     return;
   }
   try {
-    let todos = JSON.parse(data);
-    todos = Array.isArray(todos) ? todos : [];
+    const todos = JSON.parse(data);
     const completedTasks = todos.reduce((compTasks, currTask) => {
       const uid = currTask.userId;
 
-      if (isNaN(compTasks[uid])) {
+      if (isNaN(compTasks[uid]) && currTask.completed) {
         compTasks[uid] = 0;
       }
       currTask.completed && compTasks[uid]++;
