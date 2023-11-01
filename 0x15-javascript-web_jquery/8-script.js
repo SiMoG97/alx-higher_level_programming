b1619fd257614a223e3f0c79051bf964eb160c7f
SiMoG97 <echaaranimohamed@gmail.com>
2023-11-01 17:35:33 +0100

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$.get(
+  'https://swapi-api.alx-tools.com/api/films/?format=json',
+  function (data, status) {
+    if (status === 'success') {
+      data.results.forEach((movie) => {
+        $('#list_movies').append('<li>' + movie.title + '</li>');
+      });
+    }
+  }
+);
